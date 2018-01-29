c324fff5796a483cb4698194423933235f7d0a11
WedgeNix <Mullen.bryce@gmail.com>
2018-01-29 11:57:25 -0800

    updated GetProducts

--- products/structs.go
+++ products/structs.go
@@ -24,6 +24,7 @@ type GetProducts struct {
 	PageNumber                int
 	PageSize                  int
 	ProductSKUs               []string
+	ProductCodes              []string
 }
 
 // Product all data on one product
