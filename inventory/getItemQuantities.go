e79b8d84a33e870939374b3f5ab04a65dc231865
outtalinenomad <mullen.bryce@gmail.com>
2019-03-27 14:34:20 -0700

    import time

--- inventory/getItemQuantities.go
+++ inventory/getItemQuantities.go
@@ -1,24 +1,26 @@
 package inventory
 
+import "time"
+
 // GetItemQuantities is a automatically generated struct from json provided by sku vault's api docs.
 type GetItemQuantities struct {
-	ModifiedAfterDateTimeUtc  time.Time   `json:"ModifiedAfterDateTimeUtc"`
-	ModifiedBeforeDateTimeUtc time.Time   `json:"ModifiedBeforeDateTimeUtc"`
-	PageNumber                int      `json:"PageNumber"`
-	PageSize                  int      `json:"PageSize"`
-	ProductCodes              []string `json:"ProductCodes"`
+	ModifiedAfterDateTimeUtc  time.Time `json:"ModifiedAfterDateTimeUtc"`
+	ModifiedBeforeDateTimeUtc time.Time `json:"ModifiedBeforeDateTimeUtc"`
+	PageNumber                int       `json:"PageNumber"`
+	PageSize                  int       `json:"PageSize"`
+	ProductCodes              []string  `json:"ProductCodes"`
 }
 
 // GetItemQuantitiesResponse is a automatically generated struct from json provided by sku vault's api docs.
 type GetItemQuantitiesResponse struct {
 	Items []struct {
-		AvailableQuantity       int    `json:"AvailableQuantity"`
-		Code                    string `json:"Code"`
-		HeldQuantity            int    `json:"HeldQuantity"`
+		AvailableQuantity       int       `json:"AvailableQuantity"`
+		Code                    string    `json:"Code"`
+		HeldQuantity            int       `json:"HeldQuantity"`
 		LastModifiedDateTimeUtc time.Time `json:"LastModifiedDateTimeUtc"`
-		PendingQuantity         int    `json:"PendingQuantity"`
-		PickedQuantity          int    `json:"PickedQuantity"`
-		Sku                     string `json:"Sku"`
-		TotalOnHand             int    `json:"TotalOnHand"`
+		PendingQuantity         int       `json:"PendingQuantity"`
+		PickedQuantity          int       `json:"PickedQuantity"`
+		Sku                     string    `json:"Sku"`
+		TotalOnHand             int       `json:"TotalOnHand"`
 	} `json:"Items"`
 }
